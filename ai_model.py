ec3d393ae2a23766d679ab27822b4eaa8f02ee7c
ROMY87-T <abozaid.ramy44@gmail.com>
2025-07-06 01:03:44 +0300

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -4,12 +4,14 @@ def load_data():
     with open("outfit_data.json", "r") as f:
         return json.load(f)
 
-def recommend_outfits(height, weight, skin_tone):
+def recommend_outfits(height, weight, skin_tone, gender):
+
     outfits = load_data()
     recommended = []
 
     for item in outfits:
-        if skin_tone in item["skin_tones"]:
+       if skin_tone in item["skin_tones"] and item["gender"].lower() == gender.lower():
+
             recommended.append(item["name"])
 
     return recommended[:5]
